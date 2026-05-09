9194d014343b07b8622d2d7fdee7ea3a4116fbef
Shubhrato Badole <bshubhrato@gmail.com>
2026-05-09 16:46:35 +0530

    fix: force filename refresh

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import ErrorBoundary from './components/ErrorBoundary.jsx'
 import App from './App.jsx'
+import ErrorBoundary from './components/ErrorBoundary.jsx'
 import "./index.css";
 
 
