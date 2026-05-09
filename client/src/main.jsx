664f7196e37960040199ecfa3d8d7f22de757d1f
Shubhrato Badole <bshubhrato@gmail.com>
2026-05-09 16:32:24 +0530

    fix import case sensitivity

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ErrorBoundary from './components/ErrorBoundary.jsx'
 import App from './App.jsx'
 import "./index.css";
-import ErrorBoundary from './components/ErrorBoundary'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
