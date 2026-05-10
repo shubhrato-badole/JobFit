c79725b2016767c8acf73651f70746dcb1c67296
Shubhrato Badole <bshubhrato@gmail.com>
2026-05-10 10:44:34 +0530

    updated callback url

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -387,7 +387,6 @@ router.post("/logout", async  (req, res) => {
 
    // google login aouth 
 console.log("Google strategy loaded")
-console.log(process.env.GOOGLE_CALLBACK_URL)
 console.log("CLIENT ID:", process.env.GOOGLE_CLIENT_ID)
 console.log("CALLBACK:", process.env.GOOGLE_CALLBACK_URL)
 
@@ -396,7 +395,7 @@ passport.use(new GoogleStrategy({
 
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL:"https://jobfit-guyz.onrender.com/api/auth/google/callback",
+  callbackURL:process.env.GOOGLE_CALLBACK_URL,
   
 
 },
