91c5b85c7271e50cf2617ac895555265a1c0f2d5
Shubhrato Badole <bshubhrato@gmail.com>
2026-06-14 01:10:13 +0530

    fix: use separate api key for chat

--- server/src/routes/ChatBot.js
+++ server/src/routes/ChatBot.js
@@ -6,7 +6,7 @@ import Authorization from '../middleware/authmiddelware.js'
 const router  = express.Router()
 const callGemini = async (contents) => {
   const res = await fetch(
-    `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+    `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.GEMINI_CHATBOT_KEY}`,
     {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
